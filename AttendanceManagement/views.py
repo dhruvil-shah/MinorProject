03e59858ef872013e93f6c70cb18eaf9a3db23af
Dhruvil Shah <dhruvilshah2001@gmail.com>
2022-08-29 21:26:15 +0530

    Added Image adding part

--- AttendanceManagement/views.py
+++ AttendanceManagement/views.py
@@ -1,5 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import cv2
+import os
 
 def home(request):
     return render(request,'home.html')
+def addImages(request,id):
+    path='..\..\static'
+    cam_port = 0
+    cam = cv2.VideoCapture(cam_port)
+    img_path=os.path.join(path,id+".png")
+    print(img_path)
+    while True:
+        result, image = cam.read()
+        key=cv2.waitKey(1)
+        cv2.imshow(id, image)
+        if key==27:
+            cv2.imwrite(id+".png", image)
+            cv2.destroyAllWindows()
+            break
+    return HttpResponse("Getting Your Image")
